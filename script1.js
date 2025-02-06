3d8f8720bf7e23df00ebe2646ded4e0d9aa885b7
dharvi-22 <dharvijit.dj@gmail.com>
2025-02-06 15:41:10 +0000

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,16 @@
+document.write("HELLO, WORLD!");
+document.getElementsByTagName("h1")[0].innerHTML = name 
+
+//conditionals 
+var year = 0;
+
+if (year === 1) {
+  console.log("you are in year 1")
+}
+else if (year ===2){
+    console.log("you are in year 2")
+}
+
+else{
+    console.log("I don't know what year you are in")
+}
